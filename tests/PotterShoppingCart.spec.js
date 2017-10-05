6cb2e7a0257beb449996c17ffa09ee7140beb566
jame2408 <jame2408@gmail.com>
2017-10-05 11:57:16 +0800

    refactor test case.

--- tests/PotterShoppingCart.spec.js
+++ tests/PotterShoppingCart.spec.js
@@ -7,17 +7,19 @@ chai.should();
 describe('Buy Harry Potter Books', () => {
     describe('#CheckOut', () => {
         it('Test_checkout_0_books_amount_should_be_0', () => {
-            // Arrange
             var expected = 0;
-            var actual = 0;
-            var cart = new PotterShoppingCart();
             var potterBooks = [];
 
-            // Act
-            actual = cart.Checkout(potterBooks);
-            
-            // Assert
-            actual.should.equal(expected);
+            amountShouldBe(expected, potterBooks);
         });
+
+        var amountShouldBe = function (expected, books) {
+            var actual = 0;
+
+            var cart = new PotterShoppingCart();
+            actual = cart.Checkout(books);
+            
+            actual.should.equal(expected);
+        }
     });
 });
